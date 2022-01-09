95dbd93ca677f198f82365194ba86a93be916e65
Luke <85101495+lukewozniak@users.noreply.github.com>
2022-01-09 20:34:57 +0100

    Updated Czesc.jsx Krok.jsx

---
+++ src/Czesc.jsx
@@ -0,0 +1,29 @@
+const Czesc = () => {
+    return (<div className="body">
+    <header className="head head1">
+        <img className="orion orion1" src="img/2.svg" alt="orion"/>
+    </header>
+    <div className="container">
+      <div class="bigbox">
+          <div class="des">
+              <div class="progress">
+              </div>
+              <div class="greeting">
+                  <p class="hello">
+                      Cześć Bartosz!
+                  </p>
+                  <p class="road">
+                      Dopiero zaczynasz ścieżkę UX, ale już wkrótce ..
+                  </p>
+              </div>
+          </div>
+          <div class="ux">
+              <img src="img/3704081.jpg" alt="minds" class="minds"></img>
+          </div>
+      </div>
+    </div>
+</div>)
+};
+
+
+export default Czesc
