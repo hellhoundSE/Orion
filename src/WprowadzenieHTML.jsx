35883f2bdbf61ea75dc326508c12d7203ad98dc1
Luke <85101495+lukewozniak@users.noreply.github.com>
2022-01-11 23:53:44 +0100

    Added ProgressBar

--- src/WprowadzenieHTML.jsx
+++ src/WprowadzenieHTML.jsx
@@ -7,6 +7,9 @@ import Button from '@mui/material/Button';
 import Menu from '@mui/material/Menu';
 import MenuItem from '@mui/material/MenuItem';
 import Fade from '@mui/material/Fade';
+import "react-step-progress-bar/styles.css";
+import { ProgressBar, Step } from "react-step-progress-bar";
+
 
 const WprowadzenieHTML = () => {
   const navigate = useNavigate();
@@ -38,8 +41,60 @@ const WprowadzenieHTML = () => {
     </header>*/}
     <Header></Header>
     {/*<p class="sci">{sessionStorage.getItem('user')}</p>*/}
-
+    <div className="progressBar">
+    <ProgressBar
+        percent={25}
+        fillBackground="linear-gradient(to right, #fefb72, #f0bb31)"
+      >
+        <Step transition="scale">
+          {({ accomplished }) => (
+            <img
+              style={{ filter: `grayscale(${accomplished ? 0 : 80}%)` }}
+              width="30"
+              src="https://vignette.wikia.nocookie.net/pkmnshuffle/images/9/9d/Pichu.png/revision/latest?cb=20170407222851"
+            />
+          )}
+        </Step>
+        <Step transition="scale">
+          {({ accomplished }) => (
+            <img
+              style={{ filter: `grayscale(${accomplished ? 0 : 80}%)` }}
+              width="30"
+              src="https://vignette.wikia.nocookie.net/pkmnshuffle/images/9/97/Pikachu_%28Smiling%29.png/revision/latest?cb=20170410234508"
+            />
+          )}
+        </Step>
+        <Step transition="scale">
+          {({ accomplished }) => (
+            <img
+              style={{ filter: `grayscale(${accomplished ? 0 : 80}%)` }}
+              width="30"
+              src="https://orig00.deviantart.net/493a/f/2017/095/5/4/raichu_icon_by_pokemonshuffle_icons-db4ryym.png"
+            />
+          )}
+        </Step>
+        <Step transition="scale">
+          {({ accomplished }) => (
+            <img
+              style={{ filter: `grayscale(${accomplished ? 0 : 80}%)` }}
+              width="30"
+              src="https://orig00.deviantart.net/493a/f/2017/095/5/4/raichu_icon_by_pokemonshuffle_icons-db4ryym.png"
+            />
+          )}
+        </Step>
+        <Step transition="scale">
+          {({ accomplished }) => (
+            <img
+              style={{ filter: `grayscale(${accomplished ? 0 : 80}%)` }}
+              width="30"
+              src="https://orig00.deviantart.net/493a/f/2017/095/5/4/raichu_icon_by_pokemonshuffle_icons-db4ryym.png"
+            />
+          )}
+        </Step>
+      </ProgressBar>
+    </div>
     <div className="containerContent">
+
       <div className="introduction">
         <div className="progressPanel">
           <p>MENU</p>
