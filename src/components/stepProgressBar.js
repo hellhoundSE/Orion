7086456ee98acf2d3a6c68bf7c488d60b15f4515
StanleySroczynsky <80903735+StanleySroczynsky@users.noreply.github.com>
2022-01-12 23:35:54 +0100

    Progress Bar Added Proper Colours
    Commit to progress bar. Colours are already changing according to percent property

--- src/components/stepProgressBar.js
+++ src/components/stepProgressBar.js
@@ -9,50 +9,50 @@ export const StepProgressBar = () => {
 
       <ProgressBar
           percent={25}
-          fillBackground="linear-gradient(to right, #fefb72, #f0bb31)"
+          filledBackground="linear-gradient(to right, #ff9296, #ff9296)"
         >
           <Step transition="scale">
             {({ accomplished }) => (
               <img
-                style={{ filter: `grayscale(${accomplished ? 0 : 80}%)` }}
+                style={{ filter: `grayscale(${accomplished ? 0 : 100}%)` }}
                 width="30"
-                src="https://vignette.wikia.nocookie.net/pkmnshuffle/images/9/9d/Pichu.png/revision/latest?cb=20170407222851"
+                src="img/koloCzerwone.png"
               />
             )}
           </Step>
           <Step transition="scale">
             {({ accomplished }) => (
               <img
-                style={{ filter: `grayscale(${accomplished ? 0 : 80}%)` }}
+                style={{ filter: `grayscale(${accomplished ? 0 : 100}%)` }}
                 width="30"
-                src="https://vignette.wikia.nocookie.net/pkmnshuffle/images/9/97/Pikachu_%28Smiling%29.png/revision/latest?cb=20170410234508"
+                src="img/koloCzerwone.png"
               />
             )}
           </Step>
           <Step transition="scale">
             {({ accomplished }) => (
               <img
-                style={{ filter: `grayscale(${accomplished ? 0 : 80}%)` }}
+                style={{ filter: `grayscale(${accomplished ? 0 : 100}%)` }}
                 width="30"
-                src="https://orig00.deviantart.net/493a/f/2017/095/5/4/raichu_icon_by_pokemonshuffle_icons-db4ryym.png"
+                src="img/koloCzerwone.png"
               />
             )}
           </Step>
           <Step transition="scale">
             {({ accomplished }) => (
               <img
-                style={{ filter: `grayscale(${accomplished ? 0 : 80}%)` }}
+                style={{ filter: `grayscale(${accomplished ? 0 : 100}%)` }}
                 width="30"
-                src="https://orig00.deviantart.net/493a/f/2017/095/5/4/raichu_icon_by_pokemonshuffle_icons-db4ryym.png"
+                src="img/koloCzerwone.png"
               />
             )}
           </Step>
           <Step transition="scale">
             {({ accomplished }) => (
               <img
-                style={{ filter: `grayscale(${accomplished ? 0 : 80}%)` }}
+                style={{ filter: `grayscale(${accomplished ? 0 : 100}%)` }}
                 width="30"
-                src="https://orig00.deviantart.net/493a/f/2017/095/5/4/raichu_icon_by_pokemonshuffle_icons-db4ryym.png"
+                src="img/koloCzerwone.png"
               />
             )}
           </Step>
